3c74121d7b8209c4787ba01a8b20571953581c98
Dhruva Shaw <dhruvashaw@gmail.com>
2021-05-24 13:00:40 +0530

    slash updates

--- cogs/anime and vocaloids/waifu.py
+++ cogs/anime and vocaloids/waifu.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import asyncio
+from discord_slash import cog_ext, SlashContext
 
 class Waifu(commands.Cog):
     def __init__(self, bot):
@@ -8,9 +9,7 @@ class Waifu(commands.Cog):
         self.bot.dagpi = bot.dagpi
         self.description = 'Get waifu ! UwU'
     
-    @commands.command()
-    async def waifu(self, ctx):
-        '''Get wqaifu and marry them! UwU!'''
+    async def get_waifu(self):
         waifu = await self.bot.dagpi.waifu() 
         pic = waifu['display_picture']
         name = waifu['name']
@@ -24,15 +23,37 @@ class Waifu(commands.Cog):
         e.add_field(name="**:wastebasket:**",value=trash_rank,inline=True)
         e.set_image(url=pic)
         e.set_footer(text=f'React with any emoji in 30 sec to claim him/her')
-        message = await ctx.send(embed=e)
+        return e,name
+    
+    @commands.command(aliases=['w','wfu','wa'])
+    async def waifu(self, ctx):
+        '''Get random waifu and marry them! UwU!'''
+        waifu = await self.get_waifu()
+        message = await ctx.send(embed = waifu[0])
         await message.add_reaction('💓')
         
         def check(reaction, user):
-            return user != self.bot.user
+            return user != self.bot.user and message.id == reaction.message.id
         
         try:
             reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0,check=check)
-            await ctx.send(f'**{user.mention}** has *married* **{name}**! UwU :ring:')
+            await ctx.send(f'**{user.mention}** has *married* **{waifu[-1]}**! UwU :ring:')
+        except asyncio.TimeoutError:
+            pass
+    
+    @cog_ext.cog_slash(name="waifu", description='Get random waifu and marry them! UwU!')
+    async def waifu_slash(self, ctx: SlashContext):
+        '''Get random waifu and marry them! UwU!'''
+        waifu = await self.get_waifu()
+        message = await ctx.send(embed = waifu[0])
+        await message.add_reaction('💓')
+        
+        def check(reaction, user):
+            return user != self.bot.user and message.id == reaction.message.id
+        
+        try:
+            reaction, user = await self.bot.wait_for('reaction_add', timeout=30.0,check=check)
+            await ctx.send(f'**{user.mention}** has *married* **{waifu[-1]}**! UwU :ring:')
         except asyncio.TimeoutError:
             pass
         
