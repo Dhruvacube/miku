8c665d5dca5e44754f4ef64a3a0c257c5048797a
Dhruva Shaw <dhruvashaw@gmail.com>
2021-05-25 14:30:39 +0530

    update

--- cogs/developer/developer.py
+++ cogs/developer/developer.py
@@ -3,6 +3,7 @@ import io
 import os
 import subprocess as sp
 import textwrap
+import time
 import traceback
 from contextlib import redirect_stdout
 from pathlib import Path
@@ -12,6 +13,26 @@ from discord import embeds
 from discord.ext import commands
 
 
+async def create_paginator(bot, ctx, pages):
+    msg = await ctx.send(embed=pages[0])
+
+    for reaction in ["⏮️", "◀️", "⏹️", "▶️", "⏭️"]:
+        await msg.add_reaction(reaction)
+
+    await bot.state.sadd(
+        "reaction_menus",
+        {
+            "kind": "paginator",
+            "channel": msg.channel.id,
+            "message": msg.id,
+            "end": int(time.time()) + 180,
+            "data": {
+                "page": 0,
+                "all_pages": [page.to_dict() for page in pages],
+            },
+        },)
+        
+    
 class Embed(embeds.Embed):
     def __init__(self, **kwargs):
         if "colour" not in kwargs:
